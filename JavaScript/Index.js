12db6308afb8f9074b1e5ce01f236d3086b67bf7
RSakuras <ahmad.dlvan@gmail.com>
2025-06-11 14:09:03 +0300

    asd

--- JavaScript/Index.js
+++ JavaScript/Index.js
@@ -1,7 +1,7 @@
-function SayHello(){
-    alert("Just a github file, no real reason for this to actually exist");
-}
-
-function DontDoIt(){
-    alert("You did it :(");
+function LoadPage(url, containerId){
+    fetch(url)
+    .then(response => response.text())
+    .then(html => {
+        document.getElementById(containerId).innerHTML = html;
+    })
 }
